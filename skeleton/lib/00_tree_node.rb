18a2b657d68f833c432b371625211adc879cdebb
Han Chen <hanmikechen@gmail.com>
2022-08-05 12:43:25 -0400

    complete parent= method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,31 @@
 class PolyTreeNode
+    
+    
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+    def value
+        @value
+    end
+
+    def parent=(other_node)
+        self.parent.children.delete(self) unless self.parent.nil?
+        @parent = other_node
+        @parent.children << self if @parent != nil &&
+         !parent.children.include?(self)
+    end
+
+    
 
 end
